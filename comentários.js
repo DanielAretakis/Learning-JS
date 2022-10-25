51da3813e843474ef2f45c0cd6ad62e3e83cb550
DanielAretakis <daniel.aretakis@hotmail.com>
2022-10-25 17:36:59 -0300

    Comments

---
+++ comentários.js
@@ -0,0 +1,9 @@
+//  Aqui eu tenho um comentário em linha
+
+/*
+  Aqui eu tenho um comentário multi linha
+
+  O código abaixo escreve uma mensagem no devtools
+*/
+
+console.log('Bem vindos ao starter') // A partir daqui ele iria ignorar console.log('Iria ignorar')
