560b4fda6dde6f9b4e8ad5d7bcb2398d915ba499
DanielAretakis <daniel.aretakis@hotmail.com>
2022-10-25 17:54:53 -0300

    Object

---
+++ object.js
@@ -0,0 +1,16 @@
+/*
+  Object
+    * Objeto
+    * Propriedades / Atributos
+    * Funcionalidades / Métodos
+
+  { propriedade: 'valor' }
+*/
+
+console.log({
+  nome: 'Daniel',
+  idade: 21,
+  andar: function(){
+    console.log('Andar')
+  }
+})
