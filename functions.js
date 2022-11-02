0c189ddaa4a4a27afa03b866cf6403921737ac62
DanielAretakis <daniel.aretakis@hotmail.com>
2022-11-02 17:43:02 -0300

    Learning function

---
+++ functions.js
@@ -0,0 +1,14 @@
+// Criar um aplicativo de frases motivacionais
+
+// Declaration - Declaração da função
+// Function statement
+function createPhrases(){
+  console.log('Estudar é muito bom')
+  console.log('Paciência é persistência')
+  console.log('Revisão é mãe do aprendizado')
+}
+
+// Executar a função
+// Rodar, chamar, invocar
+// Execute, run, call, invoke
+createPhrases() 
